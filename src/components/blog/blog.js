04869b0c26fcb037abee7f0fead5447c5f7e1a2e
traipham <67611258+traipham@users.noreply.github.com>
2021-08-03 21:17:56 -0700

    finished blog page add post functionality, added props, need a textarea in right half

--- src/components/blog/blog.js
+++ src/components/blog/blog.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 import styles from './blog.module.css';
 import { BlogPostInterface } from "./blogPostInterface";
+import PropTypes from 'prop-types'
 /**
  * Name of Component: Blog
  * 
- * Description: This will be a daily/weekly/monthly blog area. Each post  
- */
+ * Description: This will be a daily/weekly/monthly blog area. Each post 
+ * TODO: 
+ * - Have notes/text area in the right side where we'll be typing live notes down
+ * - Have remove button on hover of the post
+*/
 
 /**
  * This is the component that is beng displyed based on inputs
@@ -13,19 +18,55 @@ import { BlogPostInterface } from "./blogPostInterface";
  * @returns 
  */
 export const DisplayBlogPost = (props) => {
-    let imgUrl = '';
+
+    const postStyle = {
+        display: 'flex',
+        flexWrap:'wrap',
+        backgroundColor: 'white',
+        border: '1px solid black',
+        borderRadius: '5px',
+        width: 'fit-content',
+        maxWidth: 210,
+        margin: 10,
+        padding: 10
+    }
+
+    const indexStyle = {
+        display: 'inline',
+        marginRight: 10,
+        border: '1px solid red',
+        borderRadius: 5,
+        padding: 5,
+        width: 'fit-content'
+    }
+
+    const dateStyle = {
+        display: 'inline',
+        border: '2px solid black',
+        padding: 5,
+        width: 'fit-content'
+    }
 
     return(
-        <div className="container" id="post-container">
-            <h3 className="header" id="date">{props.date.toString().slice(0,16)}</h3>
-            <h4 className="header" id="index">{props.index}</h4>
-            <img src={imgUrl} id="post-image" />
-            <p id="content">{props.content}</p>
+        <div className="container" id="post-container" style={postStyle}>
+            <h4 className="header" id="index" style={indexStyle}>{props.index}</h4>
+            <h3 className="header" id="date" style={dateStyle}>{props.date.toString().slice(0,16)}</h3>
+            <img src={props.image} id="post-image" width="200px" height="200px"/>
+            <br/>
+            <p id="content">"{props.content}"</p>
             <br/>
             <hr/>
-            <p id="location"><i>{props.location}</i></p>
+            <p id="location"><b>Location: </b><i>{props.location}</i></p>
         </div>
     )
+}
+
+DisplayBlogPost.propTypes = {
+    index: PropTypes.number.isRequired,
+    date: PropTypes.object.isRequired,
+    image: PropTypes.string,
+    content: PropTypes.string,
+    location: PropTypes.string
 }
 
 /**
@@ -41,7 +82,7 @@ class Blog extends React.Component {
 
         this.state = {
             addBtn: false,
-            post:[{
+            posts:[{
                 content: 'default content',
                 location: 'default location',
                 image: 'default Image',
@@ -52,8 +93,26 @@ class Blog extends React.Component {
 
         this.setStateAddButton = this.setStateAddButton.bind(this);
         this.displayAddBlogPostInterface = this.displayAddBlogPostInterface.bind(this);
+        this.displayBlogPostFunc = this.displayBlogPostFunc.bind(this);
     }
 
+
+    displayBlogPostFunc(imgFile){
+        const contentVal = document.getElementById('input-content').value;
+        const locationVal = document.getElementById('input-location').value;
+        const imgVal = imgFile;
+
+        this.setState({
+            addBtn: false,
+            posts:[...this.state.posts, {
+                content: contentVal,
+                location: locationVal,
+                image: imgVal,
+                date: new Date(),
+                index: Number(this.state.posts.length)
+            }]
+        })
+    }
     /**
      * Set the state 'addBtn' to true when 'Add a Wish' button is clicked
      */
@@ -66,7 +125,7 @@ class Blog extends React.Component {
 
     displayAddBlogPostInterface(){
         if (this.state.addBtn){
-            return <BlogPostInterface />
+            return <BlogPostInterface displayBlogPostFunc={this.displayBlogPostFunc}/>
         }
     }
 
@@ -74,12 +133,18 @@ class Blog extends React.Component {
         return (
             <div className={styles.page}>
                 <h1>My Blog</h1>
-                <button type="button" className="add-post-btn" onClick={this.setStateAddButton}>Add Post</button>
+                <h2 style={{ float: 'right'}}>My Notes. . .</h2>
+                <button type="button" className="add-post-btn" onClick={this.setStateAddButton} style={{ visibility: 'visible'}}>Add Post</button>
                 {
                     this.displayAddBlogPostInterface()
                 }
-                <div className="container" id="post-container">
-
+                <div className="container" id={styles["post-container"]}>
+                    {
+                        this.state.posts.slice(1).map((post) =>{
+                            console.log(post);
+                            return <DisplayBlogPost key={"post-"+post.index} content={post.content} location={post.location} image={post.image} date={post.date} index={post.index}/>
+                        })
+                    }
                 </div>
             </div>
         )
--- src/components/blog/blogPostInterface.js
+++ src/components/blog/blogPostInterface.js
@@ -1,21 +1,48 @@
 import React from 'react';
+import styles from './blog.module.css';
 
 export const BlogPostInterface = (props) => {
+
+    let imgUrl = '';
+
+    const handleSelectImage = (e) =>{
+        e.preventDefault();
+        const imgFile = URL.createObjectURL(e.target.files[0]);
+        imgUrl = imgFile;
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        props.displayBlogPostFunc(imgUrl);
+    }
+
     return (
-        <div className="container" id="post-interface-container">
-            <form>
-                <label id="label-img" htmlFor="input-img"><b>Select Image:</b></label>
-                <input type="file" id="input-img" name="img" accept="image/*"></input>
+        <div className="container" id={styles["post-interface-container"]}>
+            <h4 id={styles["header"]}><b>Create a Post</b></h4>
+            <form id={styles["post-interface-form"]} onSubmit={handleSubmit}>
+                <div className={styles['input-container']}>
+                    <label id="label-img" htmlFor="input-img"><b>Select Image:</b></label>
+                    <br/>
+                    <input type="file" id="input-img" name="img" accept="image/*" onChange={handleSelectImage}></input>
+                </div>
 
-                <label id="label-content" htmlFor="input-content"><b>Describe your Day: </b></label>
-                <input type="text" id="input-content" name="content"></input>
+                <div className={styles['input-container']}>
+                    <label id="label-content" htmlFor="input-content"><b>Describe your Day: </b></label>
+                    <br/>
+                    <input type="text" id="input-content" name="content"></input>
+                </div>
 
-                <label id="label-location" htmlFor="input-location"><b>Location: </b></label>
-                <input type="text" id="input-location" name="location"></input>
+                <div className={styles['input-container']}>
+                    <label id="label-location" htmlFor="input-location"><b>Location: </b></label>
+                    <br/>
+                    <input type="text" id="input-location" name="location"></input>
+                </div>
 
-
-                <button type="submit" className="submit-btn" id="submit-post">Submit</button>
-                <button type="reset" className="reset-btn" id="reset-post">Reset</button>
+                <div className={styles['input-container']}>
+                    <button type="submit" className="submit-btn" id="submit-post">Submit</button>
+                    <button type="reset" className="reset-btn" id="reset-post">Reset</button>
+                </div>
             </form>
         </div>
     )
