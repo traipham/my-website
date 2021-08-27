d31d0c8b34c89ad3574bd8221797a1a52ef7e601
traipham <67611258+traipham@users.noreply.github.com>
2021-08-26 20:20:10 -0700

    Helper file that contains multer and GridFS packages for storing images (not using right now)

---
+++ server/helper/uploadImg.js
@@ -0,0 +1,24 @@
+// Using this alongside GridFS, maybe not needed
+
+const multer =require('multer');
+const {GridFsStorage} = require('multer-gridfs-storage');
+
+const storage = new GridFsStorage(
+    {
+        url: process.env.DB,
+        options: {useNewUrlParser: true, useUnifiedTopology: true},
+        file: (req,file) => {
+            const match = ["image/png", "image/jpeg"];
+
+            if (match.indexOf(file.mimetype) === -1) {
+                const filename = `${Date.now()}-some-name-${file.originalname}`;
+            }
+            return {
+                bucketName: "photos",
+                filename: `${Date.now()}-some-name-${fileoriginalname}`
+            }
+        }
+    }
+)
+
+module.exports = multer({ storage })
