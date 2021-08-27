2bf41b9dea8fdaa0d7730d1abcb08a232a7e6182
traipham <67611258+traipham@users.noreply.github.com>
2021-08-26 20:27:40 -0700

    Added remove button to each blog posts, connected get route, add route, and remove route to speicfic functions and components

--- src/components/blog/blog.js
+++ src/components/blog/blog.js
@@ -3,6 +3,7 @@ import { withRouter } from "react-router-dom";
 import styles from './blog.module.css';
 import { BlogPostInterface } from "./blogPostInterface";
 import PropTypes from 'prop-types'
+import axios from 'axios';
 /**
  * Name of Component: Blog
  * 
@@ -20,38 +21,60 @@ import PropTypes from 'prop-types'
 export const DisplayBlogPost = (props) => {
 
     const postStyle = {
-        display: 'flex',
-        flexWrap:'wrap',
+        // display: 'flex',
+        // flexWrap:'wrap',
+        // justifyContent: "space-between",
+        // alignContent: "space-around",
         backgroundColor: 'white',
         border: '1px solid black',
         borderRadius: '5px',
-        width: 'fit-content',
+        // width: 'fit-content',
         maxWidth: 210,
         margin: 10,
         padding: 10
     }
 
     const indexStyle = {
-        display: 'inline',
-        marginRight: 10,
+        display: "inline-block",
+        float: "right",
         border: '1px solid red',
         borderRadius: 5,
         padding: 5,
-        width: 'fit-content'
+        width: 'fit-content',
+        height: 'fit-content',
     }
 
     const dateStyle = {
-        display: 'inline',
         border: '2px solid black',
         padding: 5,
-        width: 'fit-content'
+        width: 'fit-content',
     }
+
+    const btnStyle = {
+        display: "inline-block",
+        width: "fit-content",
+        height: "fit-content",
+    }
+
+
+    function handleRemoveBtn() {
+
+        const indexToDelete = props.index - 1;
+
+        axios.delete('http://localhost:5000/blog/delete/1', { data: { index: indexToDelete }})
+            .then((res) => console.log(res));
+        
+        window.location.reload();
+    }
+
+    let imgB64 = Buffer.from(props.image.data).toString('base64')
 
     return(
         <div className="container" id="post-container" style={postStyle}>
+            <button type="button" className="btn" id="blog-btn" onClick={handleRemoveBtn} style={btnStyle}>Remove</button>
             <h4 className="header" id="index" style={indexStyle}>{props.index}</h4>
             <h3 className="header" id="date" style={dateStyle}>{props.date.toString().slice(0,16)}</h3>
-            <img src={props.image} id="post-image" width="200px" height="200px"/>
+            <img src={`data:image/jpeg;base64,${imgB64}`} id="post-image" width="200px" height="200px"/>
             <br/>
             <p id="content">"{props.content}"</p>
             <br/>
@@ -64,7 +87,7 @@ export const DisplayBlogPost = (props) => {
 DisplayBlogPost.propTypes = {
     index: PropTypes.number.isRequired,
     date: PropTypes.object.isRequired,
-    image: PropTypes.string,
+    image: PropTypes.object,
     content: PropTypes.string,
     location: PropTypes.string
 }
@@ -85,7 +108,7 @@ class Blog extends React.Component {
             posts:[{
                 content: 'default content',
                 location: 'default location',
-                image: 'default Image',
+                image: {},
                 date: new Date(),
                 index: 0
             }]
@@ -96,22 +119,71 @@ class Blog extends React.Component {
         this.displayBlogPostFunc = this.displayBlogPostFunc.bind(this);
     }
 
+    componentDidMount(){
+        const posts = axios.get('http://localhost:5000/blog/').then((res)=> {return res.data[0].blogPosts});
+        posts.then((arr) => {
+            // console.log(arr);
+            let counter = 1;
+            arr.forEach((post)=>{
+                this.setState({
+                    addBtn: false,
+                    posts: [...this.state.posts, {
+                        content: post.content,
+                        location: post.location,
+                        image: post.image,
+                        index: counter++,
+                        date: new Date(),
+                    }]
+                })
+            })
+        });
+        
+    }
 
-    displayBlogPostFunc(imgFile){
-        const contentVal = document.getElementById('input-content').value;
-        const locationVal = document.getElementById('input-location').value;
-        const imgVal = imgFile;
 
-        this.setState({
-            addBtn: false,
-            posts:[...this.state.posts, {
-                content: contentVal,
-                location: locationVal,
-                image: imgVal,
-                date: new Date(),
-                index: Number(this.state.posts.length)
-            }]
-        })
+    async displayBlogPostFunc(imgFile){
+        setTimeout( async() => {
+            const posts = await axios.get('http://localhost:5000/blog/').then((res) => { return res.data[0].blogPosts });
+            console.log(posts);
+            const post = posts[posts.length-1];
+            this.setState({
+                addBtn: false,
+                posts: [...this.state.posts, {
+                    content: post.content,
+                    location: post.location,
+                    image: post.image,
+                    index: this.state.posts.length,
+                    date: new Date(),
+                }]
+            })
+        }, 300);
+        // posts.then((arr) => {
+        //     const addedPost = arr[arr.length-1]
+        //     this.setState({
+        //         addBtn: false,
+        //         posts: [...this.state.posts, {
+        //             content: addedPost.content,
+        //             location: addedPost.location,
+        //             image: addedPost.image,
+        //             index: this.state.posts.length,
+        //             date: new Date(),
+        //         }]
+        //     })
+        // })
+        // const contentVal = document.getElementById('input-content').value;
+        // const locationVal = document.getElementById('input-location').value;
+        // const imgVal = imgFile.get('image');
+        // console.log(imgVal);
+        // this.setState({
+        //     addBtn: false,
+        //     posts: [...this.state.posts, {
+        //         content: contentVal,
+        //         location: locationVal,
+        //         image: imgVal,
+        //         index: this.state.posts.length,
+        //         date: new Date(),
+        //     }]
+        // })
     }
     /**
      * Set the state 'addBtn' to true when 'Add a Wish' button is clicked
@@ -125,7 +197,7 @@ class Blog extends React.Component {
 
     displayAddBlogPostInterface(){
         if (this.state.addBtn){
-            return <BlogPostInterface displayBlogPostFunc={this.displayBlogPostFunc}/>
+            return <BlogPostInterface index={this.state.posts.length} displayBlogPostFunc={this.displayBlogPostFunc}/>
         }
     }
 
@@ -133,7 +205,7 @@ class Blog extends React.Component {
         return (
             <div className={styles.page}>
                 <h1>My Blog</h1>
-                <h2 style={{ float: 'right'}}>My Notes. . .</h2>
+                <h2 style={{ float: 'right'}}>Feedback. . .</h2>
                 <button type="button" className="add-post-btn" onClick={this.setStateAddButton} style={{ visibility: 'visible'}}>Add Post</button>
                 {
                     this.displayAddBlogPostInterface()
@@ -141,7 +213,7 @@ class Blog extends React.Component {
                 <div className="container" id={styles["post-container"]}>
                     {
                         this.state.posts.slice(1).map((post) =>{
-                            console.log(post);
+                            // console.log(post);
                             return <DisplayBlogPost key={"post-"+post.index} content={post.content} location={post.location} image={post.image} date={post.date} index={post.index}/>
                         })
                     }
--- src/components/blog/blogPostInterface.js
+++ src/components/blog/blogPostInterface.js
@@ -1,42 +1,95 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './blog.module.css';
+import axios from 'axios';
 
 export const BlogPostInterface = (props) => {
+
+    const [contentInp, setContent] = useState('');
+    const [locationInp, setLocation] = useState('');
+    const [imageInp, setImage] = useState('');
+    const [indexInp, setIndex] = useState(props.index-1);
+    // const [date, setDate] = useState(new Date());
+    
 
     let imgUrl = '';
 
     const handleSelectImage = (e) =>{
         e.preventDefault();
-        const imgFile = URL.createObjectURL(e.target.files[0]);
-        imgUrl = imgFile;
+
+        const file = e.target.files[0];
+
+        console.log(e.target.files[0])
+        setImage(file);
+
+        const imgUrl = URL.createObjectURL(e.target.files[0]);
+        // Set preview image
+        document.getElementById("blog-img").src = imgUrl;
+
     }
 
-    const handleSubmit = (e) => {
+    const onChangeContent = (e) =>{
+        setContent(e.target.value);
+    }
+
+    const onChangeLocation = (e) =>{
+        setLocation(e.target.value);
+    }
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
-        props.displayBlogPostFunc(imgUrl);
+        if(props.index-1 < 0){
+            setIndex(0);
+        }
+        
+        let fd = new FormData();
+        fd.append('image', imageInp);
+        fd.append('content', contentInp);
+        fd.append('location', locationInp);
+        const config = {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        }
+
+        // const post = {
+        //     content: contentInp,
+        //     location: locationInp,
+        //     image: fd,
+        // }
+        // console.log(post.image);
+
+        const success = await axios.post('http://localhost:5000/blog/addPost', fd, config)
+
+        if (success.status === 200){
+            props.displayBlogPostFunc(fd);
+        } else {
+            console.log('Failed to add to database!');
+        }
+        // window.location.reload();
     }
 
     return (
         <div className="container" id={styles["post-interface-container"]}>
             <h4 id={styles["header"]}><b>Create a Post</b></h4>
-            <form id={styles["post-interface-form"]} onSubmit={handleSubmit}>
+            <form method="POST" id={styles["post-interface-form"]} onSubmit={handleSubmit} encType="multipart/form-data">
                 <div className={styles['input-container']}>
                     <label id="label-img" htmlFor="input-img"><b>Select Image:</b></label>
                     <br/>
-                    <input type="file" id="input-img" name="img" accept="image/*" onChange={handleSelectImage}></input>
+                    <input type="file" id="input-img" name="image" accept="image/*" onChange={handleSelectImage}></input>
+                    <img className="img" id="blog-img" src="#" alt="preview image" height="250px" width="250px"></img>
                 </div>
 
                 <div className={styles['input-container']}>
                     <label id="label-content" htmlFor="input-content"><b>Describe your Day: </b></label>
                     <br/>
-                    <input type="text" id="input-content" name="content"></input>
+                    <input type="text" id="input-content" name="content" onChange={onChangeContent}></input>
                 </div>
 
                 <div className={styles['input-container']}>
                     <label id="label-location" htmlFor="input-location"><b>Location: </b></label>
                     <br/>
-                    <input type="text" id="input-location" name="location"></input>
+                    <input type="text" id="input-location" name="location" onChange={onChangeLocation}></input>
                 </div>
 
                 <div className={styles['input-container']}>
