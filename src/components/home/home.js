3909d4072d449b5c46fa2b6b9584dd8d7d13ed02
traipham <67611258+traipham@users.noreply.github.com>
2021-08-29 22:49:33 -0700

    update home design

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import { Link } from "react-router-dom";
-import './home.css';
+import styles from './home.css';
 
 
 function Home() {
@@ -24,30 +24,30 @@ function Home() {
         }
     }
 
-    const handleClick = () =>{
+    const handleClick = (e) =>{
         document.getElementById('welcome-container').style.display = "none";
     }
 
     return(
         <div className="home">
-            <h2 className="heading" onClick={homeBtn}>Hello Me, this is your App! </h2>
+            <h2 className="heading" onClick={homeBtn}>Trai's Website</h2>
             <div className="header">
                 <nav>
                     <button type="button" className="home-btn" id="home-btn" onClick={homeBtn}>Home</button>
-                    <button className="resume" onClick={handleClick}><Link to="/resume">Resume</Link></button>
-                    <button className="interest" onClick={handleClick}><Link to="/interest">Interest</Link></button>
-                    <button className="blog" onClick={handleClick}><Link to="/blog">Blog</Link></button>
-                    <button className="wish-list" onClick={handleClick}><Link to="/wish-list">Wish List</Link></button>
-                    <button className="goals" onClick={handleClick}><Link to="/goals">Goals</Link></button>
-                    <button className="random" onClick={handleClick}><Link to="/random">Random</Link></button>
-                    <button className="setting" onClick={handleClick}><Link to="/setting">Setting</Link></button>
+                    <Link to="/resume"><button className="resume" onClick={handleClick}>Resume</button></Link>
+                    <Link to="/interest"><button className="interest" onClick={handleClick}>Interest</button></Link>
+                    <Link to="/blog"><button className="blog" onClick={handleClick}>Blog</button></Link>
+                    <Link to="/wish-list"><button className="wish-list" onClick={handleClick}>Wish List</button></Link>
+                    <Link to="/goals"><button className="goals" onClick={handleClick}>Goals</button></Link>
+                    {/* <Link to="/random"><button className="random" onClick={handleClick}>Leave a Note</button></Link>
+                    <Link to="/setting"><button className="setting" onClick={handleClick}>Setting</button></Link> */}
                 </nav>
             </div>
             {
                 handleWelcomePage()
             }
             <div className="home-container" id="welcome-container" style={styleWelcome}>
-                <h2><b>Welcome to my Page!</b></h2>
+                <h2 id="welcome-msg"><b>Welcome to my Page!</b></h2>
                 <div className="home-container" id="btn-container">
                     <button type="button" className="btn-btn" id="click-me-btn"><b>Click Me!</b></button>
                 </div>
