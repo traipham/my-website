f611b2c0e0bb7db65f89bc51bc56b81a5a68a54c
traipham <67611258+traipham@users.noreply.github.com>
2021-08-31 12:40:24 -0700

    fixed home welcome msg display, and added welcome msg (will need to update)

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -14,18 +14,38 @@ function Home() {
         document.getElementById('welcome-container').style.display = "block";
     }
 
+    /**
+     * Display welcome message when in home page
+     */
     function handleWelcomePage() {
-        // Get current link and check what page we're on
-        const href = window.location.href.toString();
-        if (href.slice(22, href.length) === ""){
+        // Get current path and check what page we're on
+        const pathName = window.location.pathname.toString();
+        console.log(pathName);
+        if (pathName === "" || pathName === "/"){
             styleWelcome.display = "block"
         } else { 
             styleWelcome.display = "none"
         }
     }
 
+    /**
+     * Turn welcome display off
+     * @param {*} e - event object for onClick of home button 
+     */
     const handleClick = (e) =>{
         document.getElementById('welcome-container').style.display = "none";
+    }
+
+    /**
+     * DIsplay a msg ( will update! )
+     * @param {*} e - event object for onClcik of Welcome button 
+     */
+    const handleWelcomeBtnOnClick = (e) =>{
+        window.alert(
+            "WELCOME! This is my own personal website and is my first website built entirely by me from the ground up!\n\n" + 
+            "Feel free to look around, but please don't submit any input. You can click on the buttons to check out the interface.\n\n" +
+             "I'll make a new welcome format soon!"
+            )
     }
 
     return(
@@ -49,7 +69,7 @@ function Home() {
             <div className="home-container" id="welcome-container" style={styleWelcome}>
                 <h2 id="welcome-msg"><b>Welcome to my Page!</b></h2>
                 <div className="home-container" id="btn-container">
-                    <button type="button" className="btn-btn" id="click-me-btn"><b>Click Me!</b></button>
+                    <button type="button" className="btn-btn" id="click-me-btn" onClick={handleWelcomeBtnOnClick}><b>Click Me!</b></button>
                 </div>
             </div>
         </div>
